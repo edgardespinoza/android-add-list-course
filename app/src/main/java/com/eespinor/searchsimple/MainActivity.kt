92291a5f23234ba5d5f14ed80befc421f4891a0a
eespinor <edgard.espinoza.rivas@gmail.com>
2023-08-23 11:45:27 -0500

    first commit

---
+++ app/src/main/java/com/eespinor/searchsimple/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.eespinor.searchsimple
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.eespinor.searchsimple.ui.theme.SearchSimpleTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SearchSimpleTheme {
+
+                var name by remember {
+                    mutableStateOf("")
+                }
+
+                var names by remember {
+                    mutableStateOf(listOf<String>())
+                }
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                ) {
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        OutlinedTextField(
+                            value = name,
+                            onValueChange = { text ->
+                                name = text
+                            },
+                            modifier = Modifier.weight(1f),
+                            textStyle = TextStyle(color = Color.Black)
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Button(onClick = {
+                            if (name.isNotBlank()) {
+                                names = names + name
+                                name = ""
+                            }
+                        }) {
+                            Text(text = "Add")
+                        }
+                    }
+
+                    NameList(names = names)
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun NameList(
+    names: List<String>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier) {
+        items(names) { currentName ->
+            Text(
+                text = currentName,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+            Divider()
+        }
+    }
+}
